071439d73b0613bfcc50cf3c8eddee5cd8154395
VinayTShetty <vinaytshetty@gmail.com>
2023-02-01 13:50:51 +0530

    Using a common Rule. 1)Its mainly used to write the Logic for @Before,@After in a common class.

--- UnitTesting/app/src/test/java/com/example/unittesting/UtilTest.kt
+++ UnitTesting/app/src/test/java/com/example/unittesting/UtilTest.kt
@@ -10,16 +10,13 @@ import org.junit.Assert.*
 
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class UtilTest {
 
-    private val dispatcher= StandardTestDispatcher()
-
-    @Before
-    fun setUp() {
-        Dispatchers.setMain(dispatcher)
-    }
+    @get:Rule
+    val mainCoroutineRule=MainCoroutineRule()
 
     @After
     fun tearDown() {
@@ -28,12 +25,7 @@ class UtilTest {
 
     @Test
     fun getUserName() {
-        /**
-         * To avoid waiting time on the delay function in the coroutine.runTest can be used.
-         * As its a part of the library
-           testImplementation 'org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4'
-         */
-        val sut=Util(dispatcher)
+        val sut=Util(mainCoroutineRule.testDispatcher)
         runTest {
             sut.getUserName()
         }
@@ -45,7 +37,7 @@ class UtilTest {
          * Ater running the Test case.
          * Error :- Module with the Main dispatcher had failed to initialize. For tests Dispatchers.setMain from kotlinx-coroutines-test module can be used
          */
-        val sut=Util(dispatcher)
+        val sut=Util(mainCoroutineRule.testDispatcher)
         runTest {
             sut.getuser()
         }
@@ -53,11 +45,7 @@ class UtilTest {
 
     @Test
     fun getAddress(){
-        /**
-         * Ater running the Test case.
-         * Error :- Module with the Main dispatcher had failed to initialize. For tests Dispatchers.setMain from kotlinx-coroutines-test module can be used
-         */
-        val sut=Util(dispatcher)
+        val sut=Util(mainCoroutineRule.testDispatcher)
         runTest {
             sut.getAddress()
         }
@@ -65,8 +53,3 @@ class UtilTest {
 }
 
 
-/**
- * https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-test/kotlinx.coroutines.test/-standard-test-dispatcher.html
- * Notes:- In the application if there is a main dispacther used anywhere ,Don t change it.
- * when ever there is a request for the Dispatcher use this Dispatcher.
- */
