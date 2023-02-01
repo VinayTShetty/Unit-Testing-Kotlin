aa0e94b9dc56e7d6b8dbeb08773dd9ef53838909
VinayTShetty <vinaytshetty@gmail.com>
2023-02-01 09:00:38 +0530

    Delay function Coroutine Testing 'Time Duration' can be avoided using the special function 'runTesting'

--- UnitTesting/app/src/test/java/com/example/unittesting/UtilTest.kt
+++ UnitTesting/app/src/test/java/com/example/unittesting/UtilTest.kt
@@ -1,6 +1,7 @@
 package com.example.unittesting
 
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 
 import org.junit.After
@@ -20,10 +21,12 @@ class UtilTest {
     @Test
     fun getUserName() {
         /**
-         * Drawback on this is,It will take the delay of around 10 seconds from the method which is needed to be tested.
+         * To avoid waiting time on the delay function in the coroutine.runTest can be used.
+         * As its a part of the library
+           testImplementation 'org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4'
          */
         val sut=Util()
-        runBlocking {
+        runTest {
             sut.getUserName()
         }
     }
